294d6d7d1438b8ec7e50b78e1a0161b4717acd73
WuPaulo <paulowbusiness@gmail.com>
2021-05-08 01:00:18 -0400

    jquery layout

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("hello");
+const app = {};
+
+app.getPokemon = () => {
+  console.log("hello");
+
+  $.ajax({
+    url: `https://pokeapi.co/api/v2/pokemon/`,
+    method: "GET",
+    dataType: "json",
+  }).then((result) => {
+    app.pokemonOptions(result);
+  });
+};
+
+app.pokemonOptions = (pokemonInfo) => {
+  console.log(pokemonInfo);
+  for (let pokemans in pokemonInfo.results) {
+    console.log(`${pokemans}`);
+  }
+};
+
+app.init = function () {
+  app.getPokemon();
+};
+
+$(function () {
+  app.init();
+});
